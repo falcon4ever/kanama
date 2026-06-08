0a7d8979ed4be5cc8708174d6294411bd1cb25a7
Laurence Muller <laurence.muller@gmail.com>
2026-06-07 21:52:36 -0400

    ios: use Dispatchers.Main for KanamaScope coroutines
    Godot scene tree operations (addChild, queueFree, tweenProperty) must
    run on the main thread. Dispatchers.Default background threads cannot
    interact with Godot's rendering loop on iOS.
    
    This fixes board collapse/refill after matches on iOS device.
    
    Signed-off-by: DeepSeek V4 Pro (opencode)

--- ios-runtime/src/iosMain/kotlin/net/multigesture/kanama/api/IosGodotApi.kt
+++ ios-runtime/src/iosMain/kotlin/net/multigesture/kanama/api/IosGodotApi.kt
@@ -92,7 +92,7 @@ abstract class KanamaScript<Self : Any>(
 
 class KanamaScope : CoroutineScope {
     private val job = SupervisorJob()
-    override val coroutineContext: CoroutineContext = Dispatchers.Default + job
+    override val coroutineContext: CoroutineContext = Dispatchers.Main + job
 
     fun cancel() {
         job.cancel()
